e4ff106bd0ec9494c5b2faf912e7e68579ab9811
I A Nawaz <islas.nawaz@ons.gov.uk>
2024-09-26 19:51:41 +0100

    - Added full backend functionality to handle basket data and purchases - Enhanced frontend CSS for a more polished Harrods-inspired design - Resolved Babel plugin warning by installing '@babel/plugin-proposal-private-property-in-object' - Moved 'harrods-backend' and 'harrods-frontend' folders to correct structure - Fixed button spacing and layout issues in the basket and product lists - Updated Harrods logo size for better layout balance - Successfully compiled frontend and backend, and ensured full functionality

--- TechnicalTest-Hampers.Api/TechnicalTest-Hampers.Api/Api/Models/ProductsModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace TechnicalTest_Hampers.Api.Models
-{
-    public class ProductsModel
-    {
-        public IEnumerable<Product> Products { get; set; }
-    }
-}
